e1cd05af217786bec3cdd1eec058de486425083e
Tiffany Massela <tiffanymassela@gmail.com>
2023-08-18 09:33:25 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     hello nz costume resale
+    </div>
+  );
+}
+
+export default App;
